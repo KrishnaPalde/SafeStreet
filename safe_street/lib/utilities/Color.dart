3ec170153bead4d6dc0a76deba52dbfc39249b1e
KrishnaPalde <paldekrishna4325@gmail.com>
2024-02-03 03:04:09 +0530

    Changes in SplashScreen

---
+++ safe_street/lib/utilities/Color.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+Map<int, Color> color = {
+  50: Color.fromRGBO(6, 111, 217, .1),
+  100: Color.fromRGBO(6, 111, 217, .2),
+  200: Color.fromRGBO(6, 111, 217, .3),
+  300: Color.fromRGBO(6, 111, 217, .4),
+  400: Color.fromRGBO(6, 111, 217, .5),
+  500: Color.fromRGBO(6, 111, 217, .6),
+  600: Color.fromRGBO(6, 111, 217, .7),
+  700: Color.fromRGBO(6, 111, 217, .8),
+  800: Color.fromRGBO(6, 111, 217, .9),
+  900: Color.fromRGBO(6, 111, 217, 1),
+};
