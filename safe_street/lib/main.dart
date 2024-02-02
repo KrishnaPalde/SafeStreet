7edfd2b906f3e6eaa926f52cdde1dae5e8cf17be
KrishnaPalde <paldekrishna4325@gmail.com>
2024-02-03 02:19:27 +0530

    Splash Screen Started. In Development Phase.

---
+++ safe_street/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:safe_street/firebase_options.dart';
+import 'package:safe_street/screens/country_splash_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: "safe_street",
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Safe Street',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const CountrySplashScreen(),
+    );
+  }
+}
